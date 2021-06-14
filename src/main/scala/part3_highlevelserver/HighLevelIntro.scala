3faf38aa6f103b52aeba11d4a40d3a06da84ad9a
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2021-06-13 22:32:30 -0500

    route and ~ operator

--- src/main/scala/part3_highlevelserver/HighLevelIntro.scala
+++ src/main/scala/part3_highlevelserver/HighLevelIntro.scala
@@ -17,9 +17,10 @@ object HighLevelIntro extends App {
   import akka.http.scaladsl.server.Directives._
 
   val simpleRoute: Route =
-    path("home") { // DIRECTIVE
-      complete(StatusCodes.OK) // DIRECTIVE
-    }
+    path("ho") { // DIRECTIVE -  filter http requests
+      complete(StatusCodes.OK) // DIRECTIVE -  specify how it responds
+    } // equivalent directives for get, put, patch, delete, head, options
+  // as it is not specified
 
   val pathGetRoute: Route =
     path("home") {
@@ -28,34 +29,32 @@ object HighLevelIntro extends App {
       }
     }
 
-  // chaining directives with ~
-
+  // chaining directives with ~ (meaning: otherwise)
+  // without it, it only process whatever is after it
   val chainedRoute: Route =
     path("myEndpoint") {
       get {
         complete(StatusCodes.OK)
       } /* VERY IMPORTANT ---> */ ~
-      post {
-        complete(StatusCodes.Forbidden)
-      }
+        post {
+          complete(StatusCodes.Forbidden)
+        }
     } ~
-    path("home") {
-      complete(
-        HttpEntity(
-          ContentTypes.`text/html(UTF-8)`,
-          """
+      path("home") {
+        complete(
+          HttpEntity(
+            ContentTypes.`text/html(UTF-8)`,
+            """
             |<html>
             | <body>
             |   Hello from the high level Akka HTTP!
             | </body>
             |</html>
           """.stripMargin
+          )
         )
-      )
-    } // Routing tree
+      } // Routing tree
 
-
-  Http().bindAndHandle(pathGetRoute, "localhost", 8080)
-
+  Http().bindAndHandle(chainedRoute, "localhost", 8080)
 
 }
