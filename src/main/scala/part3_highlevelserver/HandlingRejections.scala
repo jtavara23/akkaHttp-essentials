f51cc9ac25291ff2873325a01b646fc9d4c821be
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2021-06-25 22:22:38 -0500

    handle rejections on High Level server

--- src/main/scala/part3_highlevelserver/HandlingRejections.scala
+++ src/main/scala/part3_highlevelserver/HandlingRejections.scala
@@ -5,24 +5,18 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{MethodRejection, MissingQueryParamRejection, Rejection, RejectionHandler}
+import akka.http.scaladsl.server.{
+  MethodRejection,
+  MissingQueryParamRejection,
+  Rejection,
+  RejectionHandler
+}
 
 object HandlingRejections extends App {
 
   implicit val system = ActorSystem("HandlingRejections")
   implicit val materializer = ActorMaterializer()
   import system.dispatcher
-
-
-  val simpleRoute =
-    path("api" / "myEndpoint") {
-      get {
-        complete(StatusCodes.OK)
-      } ~
-      parameter('id) { _ =>
-        complete(StatusCodes.OK)
-      }
-    }
 
   // Rejection handlers
   val badRequestHandler: RejectionHandler = { rejections: Seq[Rejection] =>
@@ -41,35 +35,57 @@ object HandlingRejections extends App {
       path("api" / "myEndpoint") {
         get {
           complete(StatusCodes.OK)
+          //http://localhost:8080/api/myEndpoint?myParam=2 -> GET - OK
         } ~
-        post {
-          handleRejections(forbiddenHandler) { // handle rejections WITHIN
-            parameter('myParam) { _ =>
-              complete(StatusCodes.OK)
+          post {
+            handleRejections(forbiddenHandler) { // handle rejections WITHIN
+              parameter('myParam) { _ =>
+                complete(StatusCodes.OK)
+              //http://localhost:8080/api/myEndpoint?myParam=2 -> POST - OK
+              }
             }
+            //  POST http://localhost:8080/api/myEndpoint?superParam=2 -> I have encountered rejections: List(MissingQueryParamRejection(myParam))
           }
-        }
+        // PUT http://localhost:8080/api/myEndpoint?myParam=2 = -> I have encountered rejections: List(MethodRejection(HttpMethod(GET)), MethodRejection(HttpMethod(POST)))
       }
+      // GET/POST http://localhost:8080/api/mySupEnd -> I have encountered rejections: List()
+
     }
 
 //  Http().bindAndHandle(simpleRouteWithHandlers, "localhost", 8080)
 
+  /*
+   * Another way to handle REJECTIONS */
+
+  val simpleRoute =
+    path("api" / "myEndpoint") {
+      get {
+        complete(StatusCodes.OK)
+      } ~
+        parameter('id) { _ =>
+          complete(StatusCodes.OK)
+        }
+    }
+
   // list(method rejection, query param rejection)
-  implicit val customRejectionHandler = RejectionHandler.newBuilder()
+  implicit val customRejectionHandler = RejectionHandler
+    .newBuilder()
     .handle {
       case m: MissingQueryParamRejection =>
         println(s"I got a query param rejection: $m")
-        complete("Rejected query param!")
+        complete("Rejected query param!") // return a ROUTE
     }
     .handle {
       case m: MethodRejection =>
         println(s"I got a method rejection: $m")
-        complete("Rejected method!")
+        complete("Rejected method!") // return a ROUTE
     }
     .result()
 
-  // sealing a route
+  // having an implicit rejctionHnadler we can be sealing a route
 
   Http().bindAndHandle(simpleRoute, "localhost", 8080)
+//http://localhost:8080/api/myEndpoint?iod=2 -> POST -> I got a query param rejection: MissingQueryParamRejection(id)
+//http://localhost:8080/api/myEndpoint?iod=2 -> GET -> OK
 
 }
