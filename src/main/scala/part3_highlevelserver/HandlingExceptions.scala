d3a86e3f927ba8f3c0f2e3924edbd05289dd2d5a
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2021-06-25 22:35:03 -0500

    handle exceptions on High Level server

--- src/main/scala/part3_highlevelserver/HandlingExceptions.scala
+++ src/main/scala/part3_highlevelserver/HandlingExceptions.scala
@@ -19,26 +19,35 @@ object HandlingExceptions extends App {
         // directive that throws some exception
         throw new RuntimeException("Getting all the people took too long")
       } ~
-      post {
-        parameter('id) { id =>
-          if (id.length > 2)
-            throw new NoSuchElementException(s"Parameter $id cannot be found in the database, TABLE FLIP!")
+        post {
+          parameter('id) { id =>
+            if (id.length > 2)
+              throw new NoSuchElementException(
+                s"Parameter $id cannot be found in the database, TABLE FLIP!"
+              )
 
-          complete(StatusCodes.OK)
+            complete(StatusCodes.OK)
+          }
         }
-      }
     }
 
+//  / the default will handle with eror 500
   implicit val customExceptionHandler: ExceptionHandler = ExceptionHandler {
     case e: RuntimeException =>
+      //we cam do some implementation here to handle the exception
       complete(StatusCodes.NotFound, e.getMessage)
     case e: IllegalArgumentException =>
       complete(StatusCodes.BadRequest, e.getMessage)
   }
 
-
 //  Http().bindAndHandle(simpleRoute, "localhost", 8080)
 
+//http://localhost:8080/api/people - > error 404 -> Getting all the people took too long
+//http://localhost:8080/api/people?id=23342 - > error 400  -> Parameter 23342 cannot be found in the database, TABLE FLIP!
+
+  /*
+  ============ Separate way =======================
+   */
   val runtimeExceptionHandler: ExceptionHandler = ExceptionHandler {
     case e: RuntimeException =>
       complete(StatusCodes.NotFound, e.getMessage)
@@ -56,20 +65,21 @@ object HandlingExceptions extends App {
           // directive that throws some exception
           throw new RuntimeException("Getting all the people took too long")
         } ~
-        handleExceptions(noSuchElementExceptionHandler) {
-          post {
-            parameter('id) { id =>
-              if (id.length > 2)
-                throw new NoSuchElementException(s"Parameter $id cannot be found in the database, TABLE FLIP!")
+          handleExceptions(noSuchElementExceptionHandler) {
+            post {
+              parameter('id) { id =>
+                if (id.length > 2)
+                  throw new NoSuchElementException(
+                    s"Parameter $id cannot be found in the database, TABLE FLIP!"
+                  )
 
-              complete(StatusCodes.OK)
+                complete(StatusCodes.OK)
+              }
             }
           }
-        }
       }
     }
 
   Http().bindAndHandle(delicateHandleRoute, "localhost", 8080)
-
 
 }
